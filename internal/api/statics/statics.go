ad950994733beb72c816686f4f7c0ad69271cf9c
jayyuan <2218773049@qq.com>
2025-09-13 23:18:44 +0800

    register

--- internal/api/statics/statics.go
+++ internal/api/statics/statics.go
@@ -2,6 +2,9 @@ package statics
 
 import "github.com/gin-gonic/gin"
 
+func Register(c *gin.Context) {
+	c.HTML(200, "register.html", nil) // 添加注册页面处理
+}
 func Index(c *gin.Context) {
 	c.HTML(200, "index.html", nil)
 }
